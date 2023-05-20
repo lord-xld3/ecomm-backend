c576271aa1983a3e1f75d56dba40a3e48d795d50
Kyle Simmons <simmonskyleb@gmail.com>
2023-05-20 10:02:26 -0600

    Update product-routes.js

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -22,6 +22,7 @@ router.get('/', async (req, res) => {
   }
 });
 
+// get by id
 router.get('/:id', async (req, res) => {
   try {
     const productData = await Product.findByPk(req.params.id, {
@@ -45,6 +46,66 @@ router.get('/:id', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+router.post('/', async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+
+    // If there are associated tags, create the product-tag associations
+    if (req.body.tagIds && req.body.tagIds.length) {
+      await product.addTags(req.body.tagIds);
+      const productData = await Product.findByPk(product.id, {
+        include: [
+          { model: Category },
+          { 
+            model: Tag,
+            through: ProductTag,
+            as: 'tags'
+          }
+        ],
+      });
+      res.status(200).json(productData);
+    } else {
+      res.status(200).json(product);
+    }
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const product = await Product.findByPk(req.params.id);
+
+    if (!product) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    await product.update(req.body);
+
+    // If there are associated tags, update the product-tag associations
+    if (req.body.tagIds && req.body.tagIds.length) {
+      await product.setTags(req.body.tagIds);
+      const productData = await Product.findByPk(product.id, {
+        include: [
+          { model: Category },
+          { 
+            model: Tag,
+            through: ProductTag,
+            as: 'tags'
+          }
+        ],
+      });
+      res.status(200).json(productData);
+    } else {
+      res.status(200).json(product);
+    }
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 
 // delete one product by its `id` value
 router.delete('/:id', async (req, res) => {
